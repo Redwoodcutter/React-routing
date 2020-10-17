51d6d9507dd35d4d07b4375c30df03879c064726
redwoodcutter <oguzkumcular@gmail.com>
2020-10-17 20:17:58 +0300

    Creating Tool Bar

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -44,7 +44,16 @@ class Blog extends Component {
             }
         );
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">New Post</a></li>
+                            <li><a href="">Home</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
